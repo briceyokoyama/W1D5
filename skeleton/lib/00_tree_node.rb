9a77670ab0d2d85f462b43d5315e4c7f346fa756
briceyokoyama <briceyokoyama@gmail.com>
2019-03-15 15:20:34 -0700

    Complete PTN class up to dfs

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 class PolyTreeNode
     attr_reader :parent, :children, :value
     attr_writer :parent
@@ -9,13 +10,40 @@ class PolyTreeNode
     end
 
     def parent=(other_node)
-        if other_node != nil 
+        if @parent != nil
+            @parent.children.delete(self)
             @parent = other_node
-            other_node.children << self if !other_node.children.include?(self)
-        else
-            other_node.children.delete(self)
-            @parent = other_node
-            return nil
         end
+        if other_node != nil
+            @parent = other_node 
+            other_node.children << self if !other_node.children.include?(self)
+        end
+        
     end
+
+    def add_child(child_node)
+        self.children << child_node if !self.children.include?(child_node)
+        child_node.parent = self
+    end
+
+    def remove_child(child_node)
+        raise "Not a child" if !children.include?(child_node)
+        child_node.parent = nil
+        self.children.delete(child_node)
+    end
+
+    def dfs(target_value)
+        return self if self.value == target_value
+
+        self.children.each do |child|
+            target_node = child.dfs(target_value)
+            
+        end
+
+    end
+
+    def inspect
+        "<PolyNodeTree: value=#{self.value.inspect} parent=#{self.parent.inspect} children=#{self.children.inspect}>"
+    end
+
 end
